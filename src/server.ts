5840246dfffa1bde95f082fa7793115f6bfc6ec5
Luiz Nascimento <luizh.nnh@gmail.com>
2021-02-22 20:15:51 -0300

    feat: added database layer

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,17 @@ import { Application } from 'express';
 import helmet from 'helmet';
 import { OfferController } from './controllers/offer';
 import './util/module-alias';
+import * as database from './database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -23,6 +25,14 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const offerController = new OfferController();
     this.addControllers([offerController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
