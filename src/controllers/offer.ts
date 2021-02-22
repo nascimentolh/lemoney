e076de7d5363eb64f84170dc8d5afcfc20ce2ee0
Luiz Nascimento <luizh.nnh@gmail.com>
2021-02-22 19:54:24 -0300

    chore: add prettier for code style

--- src/controllers/offer.ts
+++ src/controllers/offer.ts
@@ -1,24 +1,24 @@
-import { Controller, Get } from "@overnightjs/core";
-import { Request, Response } from "express";
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
 
-@Controller("offer")
+@Controller('offer')
 export class OfferController {
   @Get()
   public getOffers(_: Request, res: Response): void {
     res.send([
       {
-        id: "da1id1289dakjd12asldasd1sa",
-        advertiser_name: "Jhon Execute",
-        url: "http://jhonexecute.tt",
-        starts_at: "2021-01-01 00:00:00",
-        ends_at: "2021-02-02 00:00:00",
+        id: 'da1id1289dakjd12asldasd1sa',
+        advertiser_name: 'Jhon Execute',
+        url: 'http://jhonexecute.tt',
+        starts_at: '2021-01-01 00:00:00',
+        ends_at: '2021-02-02 00:00:00',
         premium: false,
       },
       {
-        id: "da1id1289dakjd12aasdasassa",
-        advertiser_name: "Jhon Doe",
-        url: "http://jhondoe.tt",
-        starts_at: "2021-01-01 00:00:00",
+        id: 'da1id1289dakjd12aasdasassa',
+        advertiser_name: 'Jhon Doe',
+        url: 'http://jhondoe.tt',
+        starts_at: '2021-01-01 00:00:00',
         premium: true,
       },
     ]);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { Server } from "@overnightjs/core";
-import bodyParser from "body-parser";
-import { Application } from "express";
-import helmet from "helmet";
-import { OfferController } from "./controllers/offer";
-import "./util/module-alias";
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { Application } from 'express';
+import helmet from 'helmet';
+import { OfferController } from './controllers/offer';
+import './util/module-alias';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
