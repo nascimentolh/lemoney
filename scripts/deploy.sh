1282e82d01dd68c568c56ce2c01b9acf0d311e5a
Luiz Nascimento <luizh.nnh@gmail.com>
2021-02-23 12:36:05 -0300

    chore: modified the url for check ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.ssh
 echo -e "${SSH_KEY}//_/\\n" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/know_hosts
+ssh-keyscan -p 44901 -t rsa geonosis.ssh.umbler.com 2>&1 >> ~/.ssh/know_hosts
 
 
